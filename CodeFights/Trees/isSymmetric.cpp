44bfb3dae0340ee909ddcc390739095c3ef991a0
David Valles <david.valles@wizeline.com>
2018-03-04 12:05:38 -0600

    Added CodeFights

---
+++ CodeFights/Trees/isSymmetric.cpp
@@ -0,0 +1,12 @@
+//
+// Definition for binary tree:
+// template<typename T>
+// struct Tree {
+//   Tree(const T &v) : value(v), left(nullptr), right(nullptr) {}
+//   T value;
+//   Tree *left;
+//   Tree *right;
+// };
+bool isTreeSymmetric(Tree<int> *t) {
+
+}
