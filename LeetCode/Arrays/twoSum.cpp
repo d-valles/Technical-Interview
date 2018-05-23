3d7067641c6135252b848064525fcecc05e84829
David Valles <david.valles@wizeline.com>
2018-05-23 13:38:26 -0500

    Submitted

--- LeetCode/Arrays/twoSum.cpp
+++ LeetCode/Arrays/twoSum.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    // sort
+    vector<int> twoSumSortedArray(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end())
+
+        int start = 0;
+        int end = nums.end();
+
+        while(start < end) {
+            int sum = nums[start] + nums[end];
+            if (sum == target) {
+                return {nums[start], nums[end]};
+            } else if (sum > target) {
+                end--;
+            }else {
+                start++;
+            }
+        }
+        
+        return {};
+    }
+    // desordenado
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> hashMap;
         int lookingFor = 0;
